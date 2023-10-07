e1d22326f49b45a0e03d21caeab347d55e7b5be3
ZueffC <zueffc@gmail.com>
2023-10-07 19:21:24 +0300

    Were made the first strokes of shit on the canvas

--- main.go
+++ main.go
@@ -1,5 +1,31 @@
 package main
 
+import (
+	"os"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
+)
+
 func main() {
-	println("Словно...")
+	app := fiber.New()
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		println(err)
+	}
+
+	app.Get("/", func(ctx *fiber.Ctx) error {
+		return nil
+	})
+
+	app.Get("/:board", func(ctx *fiber.Ctx) error {
+		return nil
+	})
+
+	app.Get("/:board/topic/:topic_id", func(ctx *fiber.Ctx) error {
+		return nil
+	})
+
+	app.Listen(":" + os.Getenv("APP_PORT"))
 }
